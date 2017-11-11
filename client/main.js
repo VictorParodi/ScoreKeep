faf6074a91d7290cb7793173005e934c85ffb484
Victor <victor.parodi.montero@gmail.com>
2017-11-11 00:55:08 -0500

    rama1 commit 1
    Utilizando Tracker. Renderizando datos desde la colección players.

--- client/main.js
+++ client/main.js
@@ -1,26 +1,8 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import { Meteor } from 'meteor/meteor';
-
-const players = [
-  {
-    _id: '1',
-    name: 'Allison',
-    score: 10
-  },
-
-  {
-    _id: '2',
-    name: 'Kate',
-    score: 8
-  },
-
-  {
-    _id: '3',
-    name: 'Susan',
-    score: 9
-  }
-];
+import { Players } from './../imports/api/players';
+import { tracker } from 'meteor/tracker';
 
 const renderPlayers = function(playersList) {
   return playersList.map(function(player) {
@@ -29,18 +11,27 @@ const renderPlayers = function(playersList) {
 }
 
 Meteor.startup(function() {
-  let title = 'Score Keep';
-  let name = 'Johnny';
-  let jsx = (
-    <div>
-      <h1> { title } </h1>
-      <p> Hello { name } </p>
-      <p> Another awesome text. </p>
-      <div>
-        { renderPlayers(players) }
-      </div>
-    </div>
-  );
+  Tracker.autorun(function() {
+    let players = Players.find().fetch();
 
-  render(jsx, document.getElementById('app'));
+    let title = 'Score Keep';
+    let name = 'Johnny';
+    let jsx = (
+      <div>
+        <h1> { title } </h1>
+        <p> Hello { name } </p>
+        <p> Another awesome text. </p>
+        <div>
+          { renderPlayers(players) }
+        </div>
+      </div>
+    );
+  
+    render(jsx, document.getElementById('app'));
+  });
+
+  Players.insert({
+    name: 'Sara',
+    score: 15
+  });
 });
