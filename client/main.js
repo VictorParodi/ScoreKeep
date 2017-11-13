a96a3769d5e42aebfeef6504820eb5387b6c79b3
Victor <victor.parodi.montero@gmail.com>
2017-11-12 23:22:44 -0500

    rama1 commit 4
    Actualizando el score del player.

--- client/main.js
+++ client/main.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import { Meteor } from 'meteor/meteor';
 import { Players } from './../imports/api/players';
-import { tracker } from 'meteor/tracker';
+import { Tracker } from 'meteor/tracker';
 
 const renderPlayers = function(playersList) {
   return playersList.map(function(player) {
     return (
       <p key={player._id}>
         {`${player.name} has ${player.score} point(s)`}
-        <button onClick={removePlayers.bind(null, player._id)}> X </button>
+        <button onClick={updatePlayers.bind(null, player._id, 1)}> + 1 </button>
+        <button onClick={updatePlayers.bind(null, player._id, -1)} > - 1 </button>
+        <button onClick={removePlayer.bind(null, player._id)}> X </button>
       </p>
     );
   });
@@ -17,7 +19,6 @@ const renderPlayers = function(playersList) {
 
 const handleSubmit = (event) => {
   event.preventDefault();
-
   let playerName = event.target.playerName.value;
 
   if (playerName) {
@@ -30,30 +31,41 @@ const handleSubmit = (event) => {
   }
 }
 
-const removePlayers = (playerId) => {
+const updatePlayers = (playerId, operator) => {
+  Players.update(
+    playerId,
+    {
+      $inc: { score: operator }
+    }
+  );
+}
+
+const removePlayer = (playerId) => {
   Players.remove(playerId);
 }
 
 Meteor.startup(function() {
+  let title = 'Score Keep';
+  let name = 'Johnny';
+
   Tracker.autorun(function() {
     let players = Players.find().fetch();
-
-    let title = 'Score Keep';
-    let name = 'Johnny';
-
+    
     let jsx = (
       <div>
         <h1> { title } </h1>
         <p> Hello { name } </p>
         <p> Another awesome text. </p>
         <div>
-          { renderPlayers(players) }
+          { 
+            renderPlayers(players)
+          }
         </div>
 
-        <form onSubmit={handleSubmit}>
-          <input type="text" name="playerName" placeholder="Type the playername" />
-          <button> Add Player </button>
-        </form>
+          <form onSubmit={handleSubmit}>
+            <input type="text" name="playerName" placeholder="Type your name" />
+            <button> Add Player </button>
+          </form>
       </div>
     );
   
